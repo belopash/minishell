3fa1605d3ec2fee0579b92f3900c7fbecce10678
Your Name <you@example.com>
2021-01-21 19:57:34 +0300

    norme

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbrock <bbrock@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/21 19:56:33 by bbrock            #+#    #+#             */
+/*   Updated: 2021/01/21 19:57:26 by bbrock           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "t_shell.h"
 #include <stdlib.h>
 #include "libft.h"
@@ -5,9 +17,9 @@
 #include "t_builtin.h"
 #include "builtins.h"
 
-int main(int argc, char **argv, char **envp)
+int	main(int argc, char **argv, char **envp)
 {
-	static t_builtin builtins[] = {
+	static t_builtin	builtins[] = {
 		(t_builtin){"echo", ft_echo},
 		(t_builtin){"cd", ft_cd},
 		(t_builtin){"pwd", ft_pwd},
@@ -15,7 +27,7 @@ int main(int argc, char **argv, char **envp)
 		(t_builtin){"export", ft_export},
 		(t_builtin){"unset", ft_unset},
 		{0, 0}};
-	t_shell *shell;
+	t_shell				*shell;
 
 	if (!(shell = new_shell(envp, builtins)))
 		return (-1);
