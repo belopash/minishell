080888b94b5c02e9d3db56140a90fa73b79d283d
Your Name <you@example.com>
2021-01-22 14:50:37 +0300

    a

--- srcs/builtins/ft_export.c
+++ srcs/builtins/ft_export.c
@@ -6,18 +6,44 @@
 /*   By: bbrock <bbrock@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 19:16:49 by bbrock            #+#    #+#             */
-/*   Updated: 2021/01/22 12:55:28 by bbrock           ###   ########.fr       */
+/*   Updated: 2021/01/22 14:50:13 by bbrock           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 #include "../includes/t_shell.h"
+#include "../includes/utils.h"
 
-int	ft_export(t_builtin *builtin, char **args)
+static int	ft_isvalid(char *env)
 {
-	if (!ft_strchr(args[1], '='))
-		return (-1);
-	if (!builtin->shell->env->add(builtin->shell->env, args[1]))
-		return (-1);
+	int i;
+
+	i = 0;
+	while (env[i] != '=')
+	{
+		if (env[i] != '_' && !ft_isalnum(env[i]))
+			return (0);
+	}
+	return (1 && i);
+}
+
+int			ft_export(t_builtin *builtin, char **args)
+{
+	int i;
+
+	i = 1;
+	while (args[i])
+	{
+		if (!ft_isvalid(args[i]))
+			return (error("export", "not a valid identifier", 1));
+	}
+	i = 1;
+	while (args[i])
+	{
+		if (!ft_strchr(args[i], '='))
+			continue ;
+		if (!builtin->shell->env->add(builtin->shell->env, args[i]))
+			return (1);
+	}
 	return (0);
 }
--- srcs/builtins/ft_unset.c
+++ srcs/builtins/ft_unset.c
@@ -6,15 +6,40 @@
 /*   By: bbrock <bbrock@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/14 19:16:49 by bbrock            #+#    #+#             */
-/*   Updated: 2021/01/22 12:53:45 by bbrock           ###   ########.fr       */
+/*   Updated: 2021/01/22 14:49:45 by bbrock           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 #include "../includes/t_shell.h"
 
-int	ft_unset(t_builtin *builtin, char **args)
+static int	ft_isvalid(char *env)
 {
-	builtin->shell->env->del(builtin->shell->env, args[1]);
+	int i;
+
+	i = 0;
+	while (env[i])
+	{
+		if (env[i] != '_' && !ft_isalnum(env[i]))
+			return (0);
+	}
+	return (1 && i);
+}
+
+int			ft_unset(t_builtin *builtin, char **args)
+{
+	int i;
+
+	i = 1;
+	while (args[i])
+	{
+		if (!ft_isvalid(args[i]))
+			return (error("export", "not a valid identifier", 1));
+	}
+	i = 1;
+	while (args[i])
+	{
+		builtin->shell->env->del(builtin->shell->env, args[i]);
+	}
 	return (0);
 }
